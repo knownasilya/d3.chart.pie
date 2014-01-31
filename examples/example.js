728bd18284bcc962988e44c04278c789bf30ba7c
Ilya Radchenko <ilya@burstcreations.com>
2014-01-31 16:02:57 -0500

    Move legend chart to own repo

--- examples/example.js
+++ examples/example.js
@@ -1,31 +1,35 @@
 (function() {
+  var data = [
+    { label: 'one', value: 2 },
+    { label: 'two', value: 5 },
+    { label: 'three', value: 1.5 },
+    { label: 'other', value: 1 }
+  ],
+  pie,
+  legend;
+
   // YOUR SAMPLE CHART USAGE GOES HERE.
-  var chart = d3.select('#vis')
+  pie = d3.select('#pie')
     .append('svg')
       .attr('width', 450)
       .attr('height', 300)
       .chart('Pie', {
         radius: 100,
         innerRadius: 80,
-        labelTemplate: '{label}: {value}'
+        labelTemplate: '{label}: {value}',
+        labels: false,
+        legend: {
+          x: 250,
+          y: 50
+        }
       });
 
-  chart.draw([
-    {
-      label: 'one',
-      value: 2
-    },
-    {
-      label: 'two',
-      value: 5
-    },
-    {
-      label: 'three',
-      value: 1.5
-    },
-    {
-      label: 'other',
-      value: 1
-    }
-  ]);
+  legend = d3.select('#legend')
+    .append('svg')
+      .attr('width', 450)
+      .attr('height', 300)
+      .chart('Legend');
+
+  pie.draw(data);
+  legend.draw(data);
 }());
